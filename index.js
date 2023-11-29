261be5b02a33c89168887762a6417c9d7f4286ab
Keith Carangelo <mail@kcaran.com>
2023-11-29 16:06:36 -0500

    Updated to run with latest version of node-sass

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 var fs = require('fs');
-var path = require('path');
 var sass = require('node-sass');
 var mime = require('mime');
 var replaceall = require('replaceall');
 
-
-var inlineImage = function(filepath, replace, done) {  
-  var mimeType = mime.lookup(filepath);
+var inlineImage = function(filepath, replace, done) {
+  'use strict';
+  var mimeType = mime.getType(filepath);
   if (mimeType !== 'image/svg+xml') {
     throw new Error('File ' + filepath + ' is not of type image/svg+xml.');
   } else {
     var data = fs.readFileSync(filepath);
     if (!data || !data.length) {
-      throw new Error('File ' + filepath + ' is empty or cannot be read')
+      throw new Error('File ' + filepath + ' is empty or cannot be read');
     }
     var result = data.toString('utf8');
     if (replace.getLength()) {
@@ -22,15 +21,16 @@ var inlineImage = function(filepath, replace, done) {
     }
     done(result);
   }
-}
+};
 
 module.exports = function() {
+  'use strict';
   return {
     'inline-svg($filename, $replace: ())': function(filename, replace, done) {
       inlineImage(filename.getValue(), replace, function(dataUrl) {
-        var encodedUrl = encodeURIComponent(dataUrl);        
+        var encodedUrl = encodeURIComponent(dataUrl);
         done(new sass.types.String('url(\'data:image/svg+xml;charset=utf-8,' + encodedUrl + '\')'));
       });
     }
-  }
-}
+  };
+};
